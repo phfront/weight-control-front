f80d16c28df057b3bd6ffa40905e3d10b2770cd1
Pedro Henrique Silva Rosa <pedro.phdois@gmail.com>
2024-06-12 17:28:03 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,38 @@
 import { WeightGraph } from "../components/WeightGraph";
 import { InsertWeight } from "../components/InsertWeight";
 import { WeightList } from "../components/WeightList";
-import styles from './styles.module.scss';
-import { useContext, useEffect } from "react";
+import styles from "./styles.module.scss";
+import { useContext, useEffect, useState } from "react";
 import { WeightControlContext } from "../contexts/WeightControlContext";
 import { InsertWeightContextProvider } from "../contexts/InsertWeightContext";
 
 export default function Index() {
-
   const context = useContext(WeightControlContext);
 
   useEffect(() => {
     context.getWeights();
-  }, [])
+  }, []);
 
+  const [c, setC] = useState("aguardando");
+
+  const test = () => {
+    setC("processando");
+    setTimeout(() => {
+      let j;
+      for (let i = 0; i < 100000000000; i++) {
+        j = i;
+      }
+      setC("finalizou" + j);
+    }, 1000);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.insert}>
+        <p>
+          <button onClick={test}>Test</button>
+        </p>
+        <p>{c}</p>
         <InsertWeightContextProvider>
           <InsertWeight />
         </InsertWeightContextProvider>
@@ -26,8 +41,11 @@ export default function Index() {
         <WeightList list={context.weights} />
       </div>
       <div className={styles.graph}>
-        <WeightGraph dates={context.graphDates} weights={context.graphWeights} />
+        <WeightGraph
+          dates={context.graphDates}
+          weights={context.graphWeights}
+        />
       </div>
     </div>
-  )
+  );
 }
